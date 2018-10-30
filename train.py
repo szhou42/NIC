7e344dfd6fd33ca445cb357f5553a479c65d1162
Siqi Miao <msq96@qq.com>
2018-10-29 20:11:43 -0500

    update

--- train.py
+++ train.py
@@ -3,8 +3,9 @@
 Created in Oct 2018
 
 """
-
+import os
 import time
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -13,6 +14,7 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 
 from models import CNN, RNN
+from utils import save_model, load_model
 
 
 DEBUG = True
@@ -20,9 +22,13 @@ DEBUG = True
 
 BATCH_SIZE = 128
 EPOCHS = 500
-current_epoch = 1
 LR = 0.0001
-momentum = 0.9 # if SGD
+#momentum = 0.9 # if SGD
+current_epoch = 1
+time_used_global = 0.0
+model_dir = './saved_model/'
+checkpoint = 5
+
 
 transform = transforms.Compose([])
 
@@ -33,15 +39,25 @@ encoder = CNN()
 decoder = RNN()
 encoder.cuda()
 decoder.cuda()
-model_paras = list(encoder.parameters()) + list(RNN.parameters())
 
+model_paras = list(encoder.parameters()) + list(RNN.parameters())
 #optimizer = optim.SGD(model_paras, lr=LR, momentum=momentum)
 optimizer = optim.Adam(model_paras, lr=LR)
+
+# load lastest model to resume training
+model_list = os.listdir(model_dir)
+if model_list:
+    state = load_model(model_dir, model_list)
+    encoder.load_state_dict(state['encoder'])
+    decoder.load_state_dict(state['decoder'])
+    optimizer.load_state_dict(state['optimizer'])
+    current_epoch = state['epoch'] + 1
+    time_used_global = state['time_used_global']
+
 criterion = nn.CrossEntropyLoss()
 
-start_time = time.time()
 for epoch in range(current_epoch, EPOCHS+1):
-    
+    start_time_epoch = time.time()
     encoder.train()
     decoder.train()
     
@@ -58,7 +74,27 @@ for epoch in range(current_epoch, EPOCHS+1):
         
         loss = criterion(generated_captions, captions)
         
-        
-        
-        
+    time_used_epoch = time.time() - start_time_epoch
+    time_used_global += time_used_epoch
+    
+    if epoch % checkpoint == 0:
+        save_model(epoch, time_used_global, optimizer, encoder, decoder)
+
+        if DEBUG:
+            break
+
+    if DEBUG:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
         
--- utils.py
+++ utils.py
@@ -4,11 +4,26 @@ Created in Oct 2018
 
 """
 
-def save_model():
-    pass
+import numpy as np
 
-def load_model():
-    pass
+import torch
+
+
+def save_model(epoch, time_used_global, optimizer, encoder, decoder):
+   state = {
+            'epoch': epoch,
+            'time_used_global': time_used_global,
+            'optimizer': optimizer.state_dict(),
+            'encoder': encoder.state_dict(),
+            'decoder': decoder.state_dict()
+            }
+   torch.save(state, open('model_'+str(epoch)+'.pth', 'wb'))
+
+def load_model(model_dir, model_list):
+    lastest_model_idx = np.argmax([int(each_model.split('_')[1][:-4]) for each_model in model_list])
+    lastest_model = model_dir + model_list[lastest_model_idx]
+    lastest_state = torch.load(open(lastest_model, 'rb'))
+    return lastest_state
 
 
 
