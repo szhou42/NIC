6f4b2b012c8ad07c2008b75de31a02bd0d4c366c
Siqi Miao <msq96@qq.com>
2018-11-17 15:01:14 -0600

    add beam_search_v2_by_siqi

--- models.py
+++ models.py
@@ -6,13 +6,11 @@ Created in Oct 2018
 
 import pickle
 import numpy as np
-import heapq
 import torch
 import torch.nn as nn
 import torchvision
 import torch.nn.utils.rnn as rnn_utils
-import math
-import pdb
+from copy import deepcopy
 
 def resnet101(pre_trained_file, pretrained=True):
     model = torchvision.models.resnet.ResNet(torchvision.models.resnet.Bottleneck, [3, 4, 23, 3])
@@ -24,7 +22,7 @@ def resnet101(pre_trained_file, pretrained=True):
 
 class CNN(nn.Module):
     
-    def __init__(self, no_word_embeddings, pre_train_dir, freeze):
+    def __init__(self, no_word_embeddings, pre_train_dir, freeze, dropout_prob):
         super(CNN, self).__init__()
 
         pretrained_cnn = resnet101(pre_train_dir, pretrained=True)
@@ -34,7 +32,18 @@ class CNN(nn.Module):
             for param in self.resnet.parameters():
                 param.requires_grad_(requires_grad=False)
 
-        self.fc_output = nn.Linear(pretrained_cnn.fc.in_features, no_word_embeddings)
+        if dropout_prob:
+            self.fc_output = nn.Sequential(
+                    nn.Linear(pretrained_cnn.fc.in_features, pretrained_cnn.fc.in_features),
+                    nn.ReLU(),
+                    nn.BatchNorm1d(pretrained_cnn.fc.in_features),
+                    nn.Dropout(p=dropout_prob),
+                    
+                    nn.Linear(pretrained_cnn.fc.in_features, no_word_embeddings)
+                    )
+        else:
+            self.fc_output = nn.Linear(pretrained_cnn.fc.in_features, no_word_embeddings)
+
 
     def forward(self, x):
         x = self.resnet(x)
@@ -46,7 +55,7 @@ class CNN(nn.Module):
 
 class RNN(nn.Module):
 
-    def __init__(self, vocab_size, no_word_embeddings, hidden_size, num_layers, pre_trained_file, freeze):
+    def __init__(self, vocab_size, no_word_embeddings, hidden_size, num_layers, pre_trained_file, freeze, dropout_prob):
         super(RNN, self).__init__()
         
         self.id2word = np.array(pickle.load(open('../preprocessed_data/idx2word', 'rb')))
@@ -63,10 +72,23 @@ class RNN(nn.Module):
             num_layers=num_layers,
             batch_first=True
         )
-        
-        self.fc_output = nn.Linear(hidden_size, vocab_size)
 
-        self.softmax = nn.Softmax()
+
+        if dropout_prob:
+            self.fc_output = nn.Sequential(
+                    nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(),
+                    nn.BatchNorm1d(hidden_size),
+                    nn.Dropout(p=dropout_prob),
+                    
+                    nn.Linear(hidden_size, vocab_size)
+                    )
+        else:
+            self.fc_output = nn.Linear(hidden_size, vocab_size)
+        
+
+        self.softmax = nn.LogSoftmax()
+        self.softmax_v2 = nn.Softmax(dim=1)
 
     def decode_idx2word(self, idx_seq):
         return self.id2word[idx_seq]
@@ -116,7 +138,7 @@ class RNN(nn.Module):
 
         return output_captions_one_batch
 
-    # don't use this yet.. has bug
+
     def beam_search_generator(self, image_embeddings, max_caption_length=20, STKidx=1, EDKidx=2):
         # Initially, there is only an empty sequence
         beam_width = 3 
@@ -132,9 +154,9 @@ class RNN(nn.Module):
             c_image_idx = c_0[:, [image_idx], :]
 
             output_caption_one_image = []
-            all_candidates = []
             for seq_idx in range(max_caption_length):
                 print('seq_idx = ', seq_idx);
+                all_candidates = []
                 for k in range(len(sequences)):
                     seq, score, h_curr, c_curr = sequences[k]
                     last_word_in_sequence = seq[len(seq) - 1]
@@ -184,3 +206,93 @@ class RNN(nn.Module):
             output_captions_one_batch.append(output_caption_one_image)
 
         return output_captions_one_batch
+
+    def beam_search_generator_v2(self, image_embeddings, beam_width=3, alpha=0, show_all=False,
+                                 max_caption_length=20, STKidx=1, EDKidx=2):
+
+        batch_size = image_embeddings.size(0)
+        _, (h_0, c_0) = self.lstm(image_embeddings.view(batch_size, 1, -1)) # h_0.shape: 1 x batch_size x hidden_size
+        output_captions_one_batch = []
+        output_scores_one_batch = []
+
+        for image_idx in range(batch_size):
+            h_image_idx = h_0[:, [image_idx], :]
+            c_image_idx = c_0[:, [image_idx], :]
+
+            word_embeddings_image_idx = self.word_embeddings(torch.tensor(STKidx).cuda()).view(1, 1, -1)
+            _, (h_image_idx, c_image_idx) = self.lstm(word_embeddings_image_idx, (h_image_idx, c_image_idx))
+
+            output_seq_idx = self.fc_output(h_image_idx).view(1, -1)
+            softmax_output = self.softmax_v2(output_seq_idx)
+            top_k_prob, top_k_idx = softmax_output.topk(beam_width)
+
+            top_k_scores = top_k_prob.log().view(-1, 1)
+            top_k_seq = [[STKidx, each] for each in top_k_idx.view(-1).tolist()]
+
+            word_embeddings_image_idx = self.word_embeddings(torch.tensor(top_k_idx).cuda()).view(beam_width, 1, -1)
+
+            h_temp = torch.ones((1, beam_width, 512)).cuda()
+            c_temp = torch.ones((1, beam_width, 512)).cuda()
+            h_temp[:] = h_image_idx.clone()
+            c_temp[:] = c_image_idx.clone()
+            h_image_idx, c_image_idx = h_temp, c_temp
+
+            for i in range(max_caption_length):
+    
+                _, (h_image_idx, c_image_idx) = self.lstm(word_embeddings_image_idx, (h_image_idx, c_image_idx))
+                output_seq_idx = self.fc_output(h_image_idx.view(beam_width, 1, 512)).view(beam_width, -1)
+                
+                softmax_output = self.softmax_v2(output_seq_idx)
+                next_top_k_prob, next_top_k_idx = softmax_output.topk(beam_width)
+                next_top_k_score = next_top_k_prob.log()
+                
+                length_flag = [1.0] * beam_width
+                for idx in range(beam_width):
+                    if top_k_seq[idx][-1] == EDKidx:
+                        next_top_k_score[idx][0] = 0.0
+                        next_top_k_idx[idx][0] = EDKidx
+                        length_flag[idx] = 0.0
+                
+                if alpha == 0:
+                    scores = (top_k_scores + next_top_k_score).view(-1)
+                else:
+                    # normalize via seq length, not very useful yet, maybe has bugs.
+                    length_factor = (torch.FloatTensor([len(each_seq)-1 for each_seq in top_k_seq]) + torch.FloatTensor(length_flag)).view(-1, 1)
+                    length_factor = length_factor.cuda()
+                    scores =  ((1.0 / length_factor**alpha) * (top_k_scores + next_top_k_score)).view(-1)
+
+                top_k_scores, top_k_idx = scores.topk(beam_width)
+                top_k_scores = top_k_scores.view(-1, 1)
+                
+                prev_top_k_seq, next_top_k_candidate = np.unravel_index(top_k_idx, next_top_k_idx.size())
+                next_top_k_idx = next_top_k_idx[prev_top_k_seq, next_top_k_candidate]
+                
+                temp_seq = []
+                for idx in range(beam_width):
+                    temp_seq.append(deepcopy(top_k_seq[prev_top_k_seq[idx]]))
+                top_k_seq = deepcopy(temp_seq)
+                finish_counts = 0
+                for idx in range(beam_width):
+                    if top_k_seq[idx][-1] == EDKidx:
+                        finish_counts += 1
+                        continue
+                    else:
+                        top_k_seq[idx].append(next_top_k_idx[idx].item())
+
+                if finish_counts == beam_width:
+                    break
+
+                h_image_idx = h_image_idx[:, prev_top_k_seq, :]
+                c_image_idx = c_image_idx[:, prev_top_k_seq, :]
+                word_embeddings_image_idx = self.word_embeddings(next_top_k_idx).view(beam_width, 1, -1)
+
+
+            # Convert word indices to actual words
+            if show_all:
+                output_captions_one_batch.append([list(self.decode_idx2word(top_k_seq[i])) for i in range(beam_width)])
+                output_scores_one_batch.append(top_k_scores.exp().view(-1).tolist())
+            else:
+                output_captions_one_batch.append(list(self.decode_idx2word(top_k_seq[0])))
+                output_scores_one_batch.append(top_k_scores[0].exp().view(-1).tolist())
+
+        return output_captions_one_batch, output_scores_one_batch
